69fca1c2c1ed8483e5517bcf547e559e71d42632
David <dlopes@nd.edu>
2013-10-05 01:11:55 -0400

    Merged with basic sounds

--- syzygy.js
+++ syzygy.js
@@ -1,5 +1,17 @@
+// PLANET CONTSTRUCTOR
+function Planet (P) {
+    
+}
+
+// ASTEROID CONSTRUCTOR
+function Asteroid (A) {
+    A = A || {}
+    this.radius = A.radius
+    this.orbit = A.orbit || {x:0, y:0, r:0, t:0}
+}
+
 $(document).ready(function () {
-    myCtx = document.getElementById("main_canvas").getContext('2d')
+    var myCtx = document.getElementById("main_canvas").getContext('2d')
     drawPlanet(myCtx, 40, 40, 30)
     initializeAudio();
     simpleTone();
