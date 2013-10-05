0712b2bf8f7d442c4a679a7c8d5f2f7ece97dcfb
David <dlopes@nd.edu>
2013-10-05 06:02:33 -0400

    Rotating asteroids

--- syzygy.js
+++ syzygy.js
@@ -3,22 +3,13 @@ function Planet (P) {
     
 }
 
-// ASTEROID CONSTRUCTOR
-function Asteroid (A) {
-    A = A || {}
-    this.radius = A.radius
-    this.orbit = A.orbit || {x:0, y:0, r:0, t:0}
-}
-
 $(document).ready(function () {
     
-    var myCanvas = document.getElementById("main_canvas")
+    initializeAudio()
+    
+    myCanvas = document.getElementById("main_canvas")
     myCanvas.width = window.innerWidth;
     myCanvas.height = window.innerHeight;
-    var myCtx = myCanvas.getContext('2d')    
-    
-    initializeAudio()
-
     var touchHandler = function (e) {
 	if(!thePlaying)
 	    simpleToneStart(200, 3, 5)
