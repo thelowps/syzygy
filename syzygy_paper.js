421dcd437f9a0e96b1d1ddba04bf076b20156ad6
David <dlopes@nd.edu>
2013-10-06 04:48:21 -0400

    Added comments, organized the code a bit

--- syzygy_paper.js
+++ syzygy_paper.js
@@ -1,5 +1,13 @@
-var layer = project.activeLayer
+/*
+ * Syzygy
+ * An interactive, fun music synthesizer.
+ *
+ * David Lopes, Nathan Wickham
+ *
+ */
 
+// This object maps orbit distances to frequencies.
+// It follows the pentatonic scale, for now
 var RADII_NOTES = {
     100: 130.81,
     200: 146.83,
@@ -15,41 +23,40 @@ var RADII_NOTES = {
     1200: 1046.5
 }
 
+// An array to keep track of all our asteroids
+var ALL_ASTEROIDS = []
+
+// The timekeeper aggregates all information relevant to 
+// the timing of orbits and pulses. All time dependent
+// operations should be based off its time property
 var timeKeeper = {
-    time: 0,
-    offset: 0,
-    speed: 0.20,
+    time: 0, // runs between 0 and 1
+    speed: 0.20, // beats per second
     minSubdivision: 16,
     minUnit: 1/16,
     
+    // tick
+    // Increment the time by the given delta
     tick: function (delta) {
 	this.time += delta*this.speed
-	this.offset = this.time - Math.floor(this.time/this.minUnit)*this.minUnit
 	while (this.time > 1) this.time -= 1
     },
     
+    // snapTime
+    // Returns the closest minimum subdivision to the time given.
     snapTime: function (t) {
-	console.log("You clicked along " + t + " of the circle")
 	var off = t-this.time
 	if (off < 0) off += 1
-	off = Math.round(off/this.minUnit)
-	off = off*this.minUnit
-	return off
+	return (Math.round(off/this.minUnit) * this.minUnit)
     }
 }
 
-function radiusToFrequency (rad) {
-    var hundred = Math.round(rad/100)
-    if (hundred > 12) hundred = 12
-    if (hundred < 1) hundred = 1
-    r = hundred*100
-    return RADII_NOTES
-}
-
 // ASTEROID CONSTRUCTOR
+// Use with the 'new' operator.
 function Asteroid (A) {
     A = A || {}
 
+    // SET UP ORBIT
     var hundred = Math.round(A.orbitRadius/100)
     if (hundred > 12) hundred = 12
     if (hundred < 1) hundred = 1
@@ -60,79 +67,85 @@ function Asteroid (A) {
 	strokeColor: 'black'
     })
     this.speed = A.speed || 1 // revolutions per second
-    this.radius = 10
     
+    // SET UP LOCATION AND MOTION VARIABLES
     this.loc = this.orbit.getNearestPoint(A.loc)
     var curveLoc = this.orbit.getNearestLocation(this.loc)
     this.initialT = (curveLoc.parameter + curveLoc.index)/curveLoc.path.curves.length
     this.initialT = timeKeeper.snapTime(this.initialT)
     this.t = 0 // will range from 0 to 1 based on how long along its revolution it is
     this.prevT = 0
+    
+    // DRAWING
+    this.radius = 10
     this.shape = new Path.Circle({
 	center: this.loc,
 	radius: this.radius,
 	fillColor: 'blue'
     })
+    this.hue = this.shape.fillColor.hue
     
+    // NOTE
     this.frequency = RADII_NOTES[orbitRadius]
     this.note = new Note (this.frequency)
     this.note.start()
-    
-    this.hue = this.shape.fillColor.hue
-    
-    // relocate
-    // Places the asteroid in the correct place in its orbit, according to its t value
+   
+    // tick
+    // The asteroid checks the timekeeper and recalculates its position.
+    // It also checks if it should play a note and change color
     this.tick = function () {
+	// Calculate the new T, relocate
 	this.prevT = this.t
 	this.t = timeKeeper.time + this.initialT
-	if (this.prevT < 1 && this.t >= 1) {
-	    this.play()
-	}
+	while(this.t >= 1.0) this.t -= 1
+	this.loc = this.orbit.getPointAt(this.t*this.orbit.length)
+	
+	// Reposition the drawing
+	this.shape.position = this.loc
+	
+	// Bring the color back to blue if its not there currently
 	if (this.shape.fillColor.hue !== this.hue) {
 	    var dist = this.shape.fillColor.hue - this.hue
-	    this.shape.fillColor.hue -= dist/10
+	    this.shape.fillColor.hue -= dist/5
 	}
-
-	var myT = this.t
-	while(myT >= 1.0) {myT -= 1}
-	this.loc = this.orbit.getPointAt(myT*this.orbit.length)
-	this.shape.position = this.loc
+	
+	// Play the note if triggered
+	if (this.prevT < 0.5 && this.t >= 0.5) {
+	    this.note.play()
+	    this.shape.fillColor = 'red'
+	}
+	
     }
-
-    // play 
-    // Plays the note and changes the color
-    this.play = function () {
-	this.note.play()
-	this.shape.fillColor = 'red'
-    }
-}
+} // close asteroid constructor
 
 
-function createAsteroid (e) {
-    var radius = (e.point-view.center).length*2
-    var speed = (e.lastPoint-e.point).length
-    
-    ast = new Asteroid({
-	orbitCenter: view.center,
-	orbitRadius: (e.point - view.center).length*2,
-	speed: 1,
-	loc: e.point
-    })
-    
-    return ast
-}
 
-ALL_ASTEROIDS = []
-function onMouseUp (e) {
-    ALL_ASTEROIDS.push( createAsteroid(e) )
-}
-
+////////////////// FRAME HANDLER ///
 function onFrame (e) {
     timeKeeper.tick(e.delta)
     for (var i = 0; i < ALL_ASTEROIDS.length; ++i) {
 	var ast = ALL_ASTEROIDS[i]
 	ast.tick()
     }
+}
+
+
+
+////////////////// EVENT HANDLERS ///
+
+function onMouseUp (e) {
+    var radius = (e.point-view.center).length*2
+    var speed = (e.lastPoint-e.point).length
+    
+    var ast = new Asteroid({
+	orbitCenter: view.center,
+	orbitRadius: radius,
+	speed: 1,
+	loc: e.point
+    })
+    
+    
+    ALL_ASTEROIDS.push( ast )
 }
 
 function onKeyDown (e) {
@@ -144,3 +157,4 @@ function onKeyDown (e) {
 	    timeKeeper.speed = 0;
     }
 }
+
